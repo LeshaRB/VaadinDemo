f240e91c238d01fb491fef3c9a12b9552ff19da3
Aliaksei Labotski <aliaksei.labotski@gmail.com>
2018-12-31 22:39:56 +0300

    Init project

---
+++ src/main/java/com/example/vaadindemo/VaadinDemoServlet.java
@@ -0,0 +1,11 @@
+package com.example.vaadindemo;
+
+import com.vaadin.flow.server.VaadinServlet;
+import com.vaadin.flow.server.VaadinServletConfiguration;
+
+import javax.servlet.annotation.WebServlet;
+
+
+@WebServlet(urlPatterns = "/*", name = "VaadinDemoServlet", asyncSupported = true)
+@VaadinServletConfiguration(ui = VaadinDemoUI.class, productionMode = false)
+public class VaadinDemoServlet extends VaadinServlet { }
