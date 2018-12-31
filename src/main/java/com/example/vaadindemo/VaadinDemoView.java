70e3b1df88a6a8ebbc876780d8b885699fa233fb
Aliaksei Labotski <aliaksei.labotski@gmail.com>
2018-12-31 23:33:42 +0300

    Add app-layout-addon

--- src/main/java/com/example/vaadindemo/VaadinDemoView.java
+++ src/main/java/com/example/vaadindemo/VaadinDemoView.java
@@ -3,17 +3,12 @@ package com.example.vaadindemo;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.page.BodySize;
-import com.vaadin.flow.component.page.Viewport;
-import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.router.Route;
 
 /**
  * The main view of the application
  */
-@Route("")
-@BodySize(height = "100vh", width = "100vw")
-@Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
+@Route(value = "", layout = MainLayout.class)
 public class VaadinDemoView extends VerticalLayout {
 
     public VaadinDemoView() {
